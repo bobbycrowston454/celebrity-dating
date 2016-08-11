07f3f2fcd9541aa64d68b28aa4f566b107fa5188
Bobby Crowston and Jackson Meyer <jackson@>
2016-08-11 14:46:46 -0700

    will add full functionality to our celebrity spouse generator with GIFs

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,28 @@ $(document).ready(function() {
   $(".inputtypes").submit(function(event) {
     var gender = $("#gender").val();
     var travel = $("#travel").val();
-
     $(".ron").hide();
     $(".jack").hide();
     $(".hillary").hide();
     $(".fat").hide();
 
-    if (gender === 'female' && travel === 'ind' || travel === 'eur') {
+    if (gender === 'female' && travel === 'ind') {
       $('.ron').show();
-    } else if (gender === 'female' && travel === 'ama' || travel === 'arc') {
+    } else if (gender === 'female' && travel === 'eur') {
+      $('.ron').show();
+    } else if (gender === 'female' && travel === 'ama') {
       $('.jack').show();
-    } else if (gender === 'male' && travel === 'ind' || travel === 'eur') {
+    } else if (gender === 'female' && travel === 'arc') {
+      $('.jack').show();
+    } else if (gender === 'male' && travel === 'ind') {
       $('.fat').show();
-    } else (gender === 'male' && travel === 'ama' || travel === 'arc') {
+    } else if (gender === 'male' && travel === 'eur') {
+      $('.fat').show();
+    } else if (gender === 'male' && travel === 'ama') {
       $('.hillary').show();
-      }
+    } else if (gender === 'male' && travel === 'arc') {
+      $('.hillary').show();
+}
 
       event.preventDefault();
       console.log(gender);
